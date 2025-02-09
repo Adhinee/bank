27e82027ad2caeeb3072fed835d2f9f4c819ea77
Adhinee <mohammedadhinee@gmail.com>
2025-02-09 23:46:18 +0530

    server updated

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");  // Import bcrypt for hashing passwords
-const AccModels = require('./models/acc');  // Import the AccModels schem
+const AccModels = require('./models/acc');  // Import the AccModels schema
 const UserModels = require("./models/userdata");
 const app = express();
 app.use(express.json());
@@ -12,6 +12,11 @@ app.use(cors());
 mongoose.connect("mongodb+srv://bank:Bank%40123@cluster0.alh1z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/bank/bankdb")
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.log("Failed to connect to MongoDB", err));
+
+// Route for the root URL '/'
+app.get("/", (req, res) => {
+    res.send("Server is running successfully on port 5000");
+});
 
 // Login Route
 app.post("/login", (req, res) => {
@@ -38,7 +43,7 @@ app.post("/login", (req, res) => {
 
 // Signup Route
 app.post("/Signup", (req, res) => {
-    const { userName, password ,id} = req.body;
+    const { userName, password, id } = req.body;
 
     // Check if the username already exists
     AccModels.findOne({ userName: userName })
@@ -50,7 +55,7 @@ app.post("/Signup", (req, res) => {
                 bcrypt.hash(password, 10)  // Generate a salt and hash the password
                     .then(hashedPassword => {
                         // Create new user with hashed password
-                        AccModels.create({ userName, password: hashedPassword,id })
+                        AccModels.create({ userName, password: hashedPassword, id })
                             .then(acc => res.json(acc))
                             .catch(err => res.status(500).json("Error creating account: " + err));
                     })
@@ -60,31 +65,25 @@ app.post("/Signup", (req, res) => {
         .catch(err => res.status(500).json("Error checking username: " + err));
 });
 
-
-
 app.post("/user", (req, res) => {
-    const { description, cash ,id ,user} = req.body;
-    
-                UserModels.create({ description , cash , id:user })
-                .then(acc => res.json(acc))
-                            
+    const { description, cash, id, user } = req.body;
+    UserModels.create({ description, cash, id: user })
+        .then(acc => res.json(acc))
+        .catch(err => res.status(500).json("Error creating user data: " + err));
 });
 
-
-
-
-app.get('/getAcc',(req,res) =>{
+app.get('/getAcc', (req, res) => {
     AccModels.find()
-    .then(bank=> res.json(bank))
-    .catch(err => res.json(err))
-})
+        .then(bank => res.json(bank))
+        .catch(err => res.json(err));
+});
 
-
-app.get('/getUser',(req,res) =>{
+app.get('/getUser', (req, res) => {
     UserModels.find()
-    .then(user=> res.json(user))
-    .catch(err => res.json(err))
-})
+        .then(user => res.json(user))
+        .catch(err => res.json(err));
+});
+
 // Start the server
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
